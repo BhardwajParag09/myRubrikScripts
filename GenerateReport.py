291e383389a1411793d4c28a765f0171bfc2941c
paragbRubrik <109232333+paragbRubrik@users.noreply.github.com>
2022-08-13 15:18:14 +0530

    Update GenerateReport.py
    This script is designed to download reports related to various events generated in Rubrik cluster for the past 24 hours in json format. You can choose the event type and generate a report accordingly.

--- GenerateReport.py
+++ GenerateReport.py
@@ -1,8 +1,9 @@
 """ 
-Python Script to generate a report for the past 24 hours in JSON format
+Python Script to generate a report for the past 24 hours
 define: 
     cluster - Rubrik CDM IP or Cluster name
     api_token - secure token from CDM (username --> API Token manager within the UI)
+    event_type - Select the type of events you want to see.
 Once set use a cron or task scheduler to run this python script 
 """
 
@@ -17,8 +18,10 @@ urllib3.disable_warnings()
 # Parameters used in the script
 # Change these to your values 
 cluster = "X.X.X.X"
-api_token = "eXXXXXXXXXXXXXX"
-
+api_token = "XXXX"
+#Type the event type for which you want to generate the log.
+#Example: Archive, Backup, Audit, Diagnostic, Fileset, Hardware, HypervServer, Recovery, Replication, Storage, System etc
+event_type = "Backup"
 
 # connect to the cluster
 rubrik=rubrik_cdm.Connect(cluster, api_token=api_token)
@@ -34,8 +37,8 @@ date_time_str = date.strftime("%Y-%m-%dT%H:%M:%S")
 
 # Find archival data
 
-baseurl = '/event/latest?event_status=Failure&event_type=Archive&before_date={}&limit=1'
-url = baseurl.format(date_time_str)
+baseurl = '/event/latest?event_status=Failure&event_type={}&before_date={}&limit=1'
+url = baseurl.format(event_type, date_time_str)
 r = rubrik.get('v1', url)
 print(r)
 
